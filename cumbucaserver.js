641adc3b3af0e23f46b620806477f95ed83096e2
PauloHerculano <paulosz@icloud.com>
2020-05-18 00:47:45 -0300

    Update cumbucaserver.js

--- cumbucaserver.js
+++ cumbucaserver.js
@@ -1,46 +1,30 @@
-const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var content;
+var Twit = require('twit');
+var config = require('./config')
+var T = new Twit(config);
 
-app.get('/nlu', async function (req, res) {
-  content = req.query.text;
-  var result = await analyzer(content);
-  res.send(result);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: '2019-07-12',
-  authenticator: new IamAuthenticator({
-    apikey: 'IMinyq6Z_c_7CebAulM_Mi4fP7JDbMUr5g8hWQYZx_kR',
-  }),
-  url: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/699e499c-020f-441c-8fc9-693b902b0325',
-});
-
-async function analyzer(content){
-var cont = content;
-var res = "Erro";
-const analyzeParams = {
-  'features': {
-    'relations': {}
-  },
-  'text': cont
-};
-
-await naturalLanguageUnderstanding.analyze(analyzeParams)
-  .then(analysisResults => {
-
-    res = JSON.stringify(analysisResults, null, 2);
-
-  })
-  .catch(err => {
-
-    console.log('error:', err);
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS, PUSH, LISTEN"
+    );
+    next();
   });
-return res;
+
+app.get('search/tweets', content,searchedData);
+
+ function searchedData(err, data, response) {
+  res.send(data);
+  console.log(data);
 }
