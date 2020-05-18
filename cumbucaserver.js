67dc682a128e70a3668754f5e9f74be911bef216
PauloHerculano <paulosz@icloud.com>
2020-05-18 01:54:16 -0300

    Update cumbucaserver.js

--- cumbucaserver.js
+++ cumbucaserver.js
@@ -26,10 +26,10 @@ app.listen(5000, function () {
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: rsR0TooAJf4l9jU2Nyjy1dqcY,
-  consumer_secret: GEYUJYbk7gDCrVfRYxksxh9l4ZVaWB2TvyC3H2YHDYiQ2hZmzr,
-  access_token_key: 1201664563500011521-QtBqkSeJe4WZcb1Q6XFIZH0oSg92Wj,
-  access_token_secret: XRMIAtESEjI91M4w7xOshmY4o375mhJemwfh7o2eSt1iO
+  consumer_key: 'rsR0TooAJf4l9jU2Nyjy1dqcY',
+  consumer_secret: 'GEYUJYbk7gDCrVfRYxksxh9l4ZVaWB2TvyC3H2YHDYiQ2hZmzr',
+  access_token_key: '1201664563500011521-QtBqkSeJe4WZcb1Q6XFIZH0oSg92Wj',
+  access_token_secret: 'XRMIAtESEjI91M4w7xOshmY4o375mhJemwfh7o2eSt1iO'
 });
 
 var content = req.query.text;
