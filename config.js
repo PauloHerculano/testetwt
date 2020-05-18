47862ad431e4cf68df41b300af91ba182ef2ccc4
PauloHerculano <paulosz@icloud.com>
2020-05-18 00:33:52 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ consumer_key:         'rsR0TooAJf4l9jU2Nyjy1dqcY',
+  consumer_secret:      'GEYUJYbk7gDCrVfRYxksxh9l4ZVaWB2TvyC3H2YHDYiQ2hZmzr',
+  access_token:         '1201664563500011521-QtBqkSeJe4WZcb1Q6XFIZH0oSg92Wj',
+  access_token_secret:  'XRMIAtESEjI91M4w7xOshmY4o375mhJemwfh7o2eSt1iO'
+}
